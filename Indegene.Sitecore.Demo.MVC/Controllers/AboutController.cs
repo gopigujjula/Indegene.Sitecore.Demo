ced46f50b8318859b4be048f78bb3f002805f368
Gopikrishna Gujjula <gopikreddy.gn@gmail.com>
2021-06-02 13:07:54 +0530

    Page data & about components

---
+++ Indegene.Sitecore.Demo.MVC/Controllers/AboutController.cs
@@ -0,0 +1,34 @@
+﻿using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Indegene.Sitecore.Demo.MVC.Models;
+using Sitecore.Data.Fields;
+using Sitecore.Web.UI.WebControls;
+
+namespace Indegene.Sitecore.Demo.MVC.Controllers
+{
+    public class AboutController : Controller
+    {       
+        public ActionResult Index()
+        {
+            //Item, bind to the model, and pass to the view
+            var model = new AboutViewModel 
+            { 
+                DataSourceItem = RenderingContext.Current?.Rendering.Item 
+            };
+            //Item, read the values, build a model and pass it
+            var item = RenderingContext.Current?.Rendering.Item;
+            if(item!=null)
+            {
+                Field title = item.Fields["Title"];
+                model.Title = title?.Value;                 
+            }
+            //Item, read the values using FieldRenderer, build a model and pass it
+            model.SubTitle = new MvcHtmlString(FieldRenderer.Render(item, "SubTitle"));
+            return View("~/Views/Builderz/About.cshtml", model);
+        }
+    }
+}
---
+++ Indegene.Sitecore.Demo.MVC/Models/AboutViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Sitecore.Data.Items;
+
+namespace Indegene.Sitecore.Demo.MVC.Models
+{
+    public class AboutViewModel
+    {
+        public Item DataSourceItem { get; set; }
+        public string Title { get; set; }
+        public MvcHtmlString SubTitle { get; set; }
+    }
+}
