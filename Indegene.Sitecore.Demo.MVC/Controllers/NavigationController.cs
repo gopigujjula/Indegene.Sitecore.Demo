148cd9869412fb926816498e98beb02cb8a59433
Gopikrishna Gujjula <gopikreddy.gn@gmail.com>
2021-06-04 14:20:14 +0530

    Header navigation

---
+++ Indegene.Sitecore.Demo.MVC/Controllers/NavigationController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Indegene.Sitecore.Demo.MVC.Models;
+using Sitecore.Kernel;
+using SitecoreKernel = Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Links.UrlBuilders;
+using Sitecore.Mvc.Presentation;
+
+namespace Indegene.Sitecore.Demo.MVC.Controllers
+{
+    public class NavigationController : Controller
+    {
+        // GET: Navigation
+        public ActionResult HeaderNavigation()
+        {
+            var model = new HeaderNavigationModel();
+            List<NavigationModel> navigations = new List<NavigationModel>();
+
+            //read a tree // build a navigation
+            var startitemPath = SitecoreKernel.Context.Site.StartPath;
+            var homeItem = SitecoreKernel.Context.Database.GetItem(startitemPath);
+            navigations.Add(GetNavigation(homeItem));
+
+            if (homeItem.HasChildren)
+            {
+                foreach (Item child in homeItem.Children)
+                {
+                    navigations.Add(GetNavigation(child));
+                }
+            }
+            model.TopNavigations = navigations;
+
+            return View("~/Views/Builderz/HeaderNavigation.cshtml", model);
+        }
+
+        public NavigationModel GetNavigation(Item item)
+        {
+            if (item != null)
+            {
+                DefaultItemUrlBuilderOptions defaultItemUrlBuilderOptions = new DefaultItemUrlBuilderOptions();
+                defaultItemUrlBuilderOptions.AlwaysIncludeServerUrl = true;
+                ItemUrlBuilder itemUrlBuilder = new ItemUrlBuilder(defaultItemUrlBuilderOptions);
+
+                return new NavigationModel()
+                {
+                    NavigationTitle = item.Fields["NavigationTitle"]?.Value,
+                    NavigationLink = itemUrlBuilder.Build(item, defaultItemUrlBuilderOptions),
+                    ActiveClass = PageContext.Current.Item.ID == item.ID ? "active" : string.Empty                    
+                };
+            }
+            return null;
+        }
+    }
+}
---
+++ Indegene.Sitecore.Demo.MVC/Models/HeaderNavigationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Indegene.Sitecore.Demo.MVC.Models
+{
+    public class HeaderNavigationModel
+    {
+        public List<NavigationModel> TopNavigations { get; set; }
+    }
+    public class NavigationModel
+    {
+        public string NavigationTitle { get; set; }
+        public string NavigationLink { get; set; }
+        public string ActiveClass { get; set; }
+    }
+}
